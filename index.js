8a15c3fc4288f92d2f68b52696955042ed72b7f6
stopvoldemort <david.squires1@gmail.com>
2017-10-09 13:38:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) {
+    return driver.toLowerCase()
+  })
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    const split_name = driver.split(" ")
+    const new_driver = {}
+    new_driver.firstName = split_name[0]
+    new_driver.lastName = split_name[1]
+    return new_driver
+  })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  })
+}
